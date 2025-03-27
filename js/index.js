cabddf25c55691761736ff35a11715c585eb85fd
Ryan mugei <Lquid1264@gmail.com>
2025-03-27 03:32:29 +0300

    nears

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 const base_url = "http://localhost:3000/characters"
 
-document.addEventListener("DOMContentLoaded,"()=> {
+document.addEventListener("DOMContentLoaded",()=> {
+const characterInfo=document.getElementById("characterInfo")
+})
 
-}
 
-)
+
+fetch(base_url)
+.then(res=> res.json())
+.then(chaeacters=>{
+    chaeacters.forEach(character => {
+        const characterContent = document.createElement('div')
+    });
+    const characterImage = document.createElement("div");
+    characterImage.src=character.image;
+    characterImage.alt=character.name;
+    characterImage.classList.add("character-image");
+
+    const name = document.createElement("name");
+    name.textContent=character.name;
+    
+    const votes =document.createElement("input");
+    votes.value= character.votes;
+
+    characterContent.appendChild(characterImage)
+    characterContent.appendChild(name)
+    characterContent.appendChild(votes)
+    characterInfo.appendChild(characterContent)
+})
+
